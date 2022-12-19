17fd84ca491780898a76428454f87d71c933f319
JavierArancibia <javier@hodl.nl>
2022-12-19 11:36:51 +0100

    Added get Task in Controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -14,13 +14,23 @@ const createTask = async (req, res) => {
     try {
         const task = await Task.create(req.body)
         await res.status(201).json(task)
-    } catch (err) {
-        res.status(500).json({ message: err})
+    } catch (error) {
+        res.status(500).json({ message: error})
     }
 }
 
-const getTask = (req, res) => {
-    res.send('get Task')
+const getTask = async (req, res) => {
+    try {
+        // const task = await Task.findById(req.params.id) // Option that also works
+        const { id } = req.params
+        const task = await Task.findOne({ _id: id })
+        if (!task) {
+            return res.status(404).json({ message: `No task with id: ${id}` }) 
+        }
+        res.status(200).json(task)
+    } catch (error) {
+        res.status(500).json({ success:false, message: error })
+    }
 }
 
 const updateTask = (req, res) => {
